f6eafb07d44781e7bd3124e91b0821cc039903fa
SparK <fabio.serra24@gmail.com>
2020-03-04 01:03:39 +0100

    Camera Rotations added, Locked mode missing Signed-off-by: SparK <fabio.serra24@gmail.com>

---
+++ Assets/CameraLook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 look = new Vector3(target.transform.position.x, this.transform.position.y, target.transform.position.z);
+        transform.LookAt(target.transform.position);
+    }
+}
