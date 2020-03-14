5682bdfff6e2a717220a0faf44ebceaa95ae9542
GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>
2020-03-14 17:42:04 +0100

    Add start menu Signed-off-by: GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void nuovaPartita(string scena)
+    {
+        SceneManager.LoadScene(scena);
+    }
+}
