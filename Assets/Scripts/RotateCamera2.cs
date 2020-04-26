8e31a8556021770c6cf413bfcf38eba0ebec59af
GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>
2020-04-26 20:21:11 +0200

    Pre-match screen V1 Signed-off-by: GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>

---
+++ Assets/Scripts/RotateCamera2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera2 : MonoBehaviour
+{
+    float smooth = 1.0f;
+    float i = 0.0f;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion target = Quaternion.Euler(20, 235 + i, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        transform.position = transform.position - new Vector3(0, 0, 0.005f);
+        i += 0.05f;
+    }
+}
