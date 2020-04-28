d6b6c2c0f16c5b7c37da373d7874c8651c7effa8
Andrea Cardias <andreacardias@yahoo.it>
2020-04-28 18:17:16 +0200

    Andrea Cardias
    Aggiunta UI texture, skin e scripts (da collegare alle variabili)
    Aggiunta menù di pausa (da fixare)

---
+++ Assets/UI in Game/UI Scripts/BoostBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostBar : MonoBehaviour
+{
+
+    public int boost;
+    public int boostMax = 100;
+
+    public GUISkin skinBarra;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boost = boostMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boost > boostMax)
+        {
+            boost = boostMax;
+        }
+
+        if (boost < 0)
+        {
+            boost = 0;
+        }
+    }
+    private void OnGUI()
+    {
+        
+        GUI.TextArea(new Rect(Screen.width - 70, Screen.height - 40, 60, 20), boost + " / " + boostMax);
+        GUI.Box(new Rect(Screen.width-50, Screen.height - 50, 20, -Screen.height / 3), "");
+
+        if(boost>0)
+        {
+            GUI.skin = skinBarra;
+            GUI.Box(new Rect(Screen.width - 50, Screen.height-50, 20, - Screen.height / 3 * (((float)boost)/(float)boostMax)), "");
+        }
+    }
+
+
+}
---
+++ Assets/UI in Game/UI Scripts/ScoreBoard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+
+public class ScoreBoard : MonoBehaviour
+{
+    public GUISkin logoRossi;
+    public GUISkin logoBlu;
+    public GUISkin scoreBoard;
+    public GUISkin punteggi;
+    public GUISkin cronometro;
+
+    public int scoreRed;
+    public int scoreBlue;
+    public Timer tempo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        scoreBlue = 0;
+        scoreRed = 0;
+        tempo = new Timer(300);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(scoreBlue<0)
+        {
+            scoreBlue = 0;
+        }
+
+        if(scoreRed<0)
+        {
+            scoreRed = 0;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = logoRossi;
+        GUI.Box(new Rect((Screen.width / 2) - 200, 10, 100, 100), "");
+        GUI.skin = logoBlu;
+        GUI.Box(new Rect((Screen.width / 2) +100, 10, 100, 100), "");
+        GUI.skin = scoreBoard;
+        GUI.Box(new Rect((Screen.width/2)-120, 10, 240, 120),"");
+        GUI.skin = punteggi;
+        GUI.TextArea(new Rect((Screen.width / 2) - 100, 50, 100, 50), scoreRed+"");
+        GUI.TextArea(new Rect((Screen.width / 2)+5, 50, 100, 50), scoreBlue+ "");
+        GUI.skin = cronometro;
+        GUI.Box(new Rect((Screen.width / 2)-25, 40, 50, 50), "5:00");
+    }
+}
