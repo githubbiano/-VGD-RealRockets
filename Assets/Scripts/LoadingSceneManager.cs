665ccaa425b6a73f22c641dabdf4225f15d65fe3
GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>
2020-05-16 18:20:43 +0200

    Added particle effect for the backpack Signed-off-by: GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>

--- Assets/Scripts/LoadingSceneManager.cs
+++ Assets/Scripts/LoadingSceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class LoadingSceneManager : MonoBehaviour
 {
+
     float startTime;
     float endTime;
     float timeToSwitchCamera;
